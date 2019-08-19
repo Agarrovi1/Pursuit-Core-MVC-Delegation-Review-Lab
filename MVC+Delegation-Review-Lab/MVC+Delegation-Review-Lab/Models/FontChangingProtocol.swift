384264af3baf9a55f506c0fe2be51ae18f8d1e2c
Angela Garrovillas <angelagarrovillas@Angelas-MacBook-Pro.local>
2019-08-19 14:57:42 -0400

    adds protocol for font changing

---
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/Models/FontChangingProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  FontChangingProtocol.swift
+//  MVC+Delegation-Review-Lab
+//
+//  Created by Angela Garrovillas on 8/19/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import Foundation
+protocol FontSettings {
+    func fontDidChange()
+}
