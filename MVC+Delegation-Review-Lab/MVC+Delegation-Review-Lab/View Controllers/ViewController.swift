a9c0cfbc966a875d207218e2d541a2b8dc5cbb9e
Angela Garrovillas <angelagarrovillas@Angelas-MacBook-Pro.local>
2019-08-19 14:18:06 -0400

    adds tableView and delegates for it

--- MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/View Controllers/ViewController.swift
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/View Controllers/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
